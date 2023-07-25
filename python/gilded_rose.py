3a0a580cdf1cc1610fea75da7fde7aab869a21e0
Melar3 <med.taha.inpt@gmail.com>
2023-07-25 22:33:10 +0200

    refactoring pass 3

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -24,22 +24,25 @@ class GildedRose(object):
                 item.sell_in -= 1
                 if item.sell_in < 0:
                     item.quality -= 2
+                continue
             
             else:
-                item.quality -= 1
-                item.quality = max(item.quality, 0)
-                item.sell_in -= 1
-                if item.sell_in < 0:
-                    item.quality -= 1
-                    item.quality = max(item.quality, 0)
+                self.normal_item(item)
 
-    def aged_brie(self, item):
-        if item.quality < 50:
-            item.quality += 1
+    def normal_item(self, item):
+        item.quality -= 1
+        item.quality = max(item.quality, 0)
         item.sell_in -= 1
         if item.sell_in < 0:
-            if item.quality < 50:
-                item.quality += 1
+            item.quality -= 1
+            item.quality = max(item.quality, 0)
+
+    def aged_brie(self, item):
+        item.quality += 1
+        item.sell_in -= 1
+        if item.sell_in < 0:
+            item.quality += 1
+        item.quality = min(item.quality, 50)
 
     def backstage_pass(self, item):
         if item.sell_in < 6:
