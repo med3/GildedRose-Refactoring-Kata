44844ba8db743a1bf1f19e95dec959999a4af47d
Melar3 <med.taha.inpt@gmail.com>
2023-07-26 00:08:00 +0200

    cleaned up gilded rose class

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -7,6 +7,9 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
+
+            item.sell_in -= 1
+
             if item.name == "Sulfuras, Hand of Ragnaros":
                 item.quality = 80
                 continue
@@ -25,38 +28,31 @@ class GildedRose(object):
                 if item.sell_in < 0:
                     item.quality -= 2
                 continue
-            
             else:
                 self.normal_item(item)
 
     def normal_item(self, item):
         item.quality -= 1
         item.quality = max(item.quality, 0)
-        item.sell_in -= 1
         if item.sell_in < 0:
             item.quality -= 1
             item.quality = max(item.quality, 0)
 
     def aged_brie(self, item):
         item.quality += 1
-        item.sell_in -= 1
         if item.sell_in < 0:
             item.quality += 1
         item.quality = min(item.quality, 50)
 
     def backstage_pass(self, item):
-        if item.sell_in < 6:
+        if item.sell_in < 0:
+            item.quality = 0
+        elif item.sell_in < 6:
             item.quality += 3
         elif item.sell_in < 11:
             item.quality += 2
         else:
             item.quality += 1
-                
-        item.sell_in -= 1
-
-        if item.sell_in < 0:
-            item.quality = 0
-
         item.quality = min(item.quality, 50)
 
 
