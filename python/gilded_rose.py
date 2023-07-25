fa0c21225bb27811b48da79d9114dc57a6b7e521
Melar3 <med.taha.inpt@gmail.com>
2023-07-25 21:15:09 +0200

    spacing conditional blocks and starting tests

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -21,8 +21,10 @@ class GildedRose(object):
                         if item.sell_in < 6:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
+
             if item.name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in = item.sell_in - 1
+                
             if item.sell_in < 0:
                 if item.name != "Aged Brie":
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -9,8 +9,44 @@ class GildedRoseTest(unittest.TestCase):
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEquals("foo", items[0].name)
 
+    def test_sulfurus(self):
+        items = [Item("Sulfuras, Hand of Ragnaros", 20, 80)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(80, items[0].quality)
         
+    def test_sulfurus_past_date(self):
+        items = [Item("Sulfuras, Hand of Ragnaros", 0, 80)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(80, items[0].quality)
+
+    def test_aged_brie_increases_in_quality(self):
+        items = [Item("Aged Brie", 20, 30)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(19, items[0].sell_in)
+        self.assertEquals(31, items[0].quality)
+
+    def test_aged_brie_hits_50_quality(self):
+        items = [Item("Aged Brie", 20, 50)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(50, items[0].quality)
+
+    def test_normal_item(self):
+        items = [Item("Rat soup", 20, 11)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(10, items[0].quality)
+
+    def test_normal_item_degrades_twice_as_fast(self):
+        items = [Item("Rat soup", 0, 11)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(9, items[0].quality)
+
 if __name__ == '__main__':
     unittest.main()
